cbcee218dfb9a21e92c71e88c8ae95a95c3d7645
ollieb578 <ollie.b578@gmail.com>
2024-05-21 14:40:49 +0100

    New formatting test.

--- ui.js
+++ ui.js
@@ -11,7 +11,12 @@ const prompt = require("prompt-sync")();
 const Grid = require("./grid");
 
 // global vars
+// alphabet array for X mappings - there's probably a better way to do this
 const alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// text colours
+const flag = '\x1b[31mF\x1b[0m';
+const green = '\x1b[32m%s\x1b[0m';
 
 class Ui {
     constructor(grid) {
@@ -23,32 +28,43 @@ class Ui {
     // reads the revealMask from the grid object, then overlays user flags onto it
     printGrid() {
         const displayGrid = this.grid.getRevealMask();
+        let line = [];
 
         console.log("\n\nMINESWEEPER\n");
 
-        process.stdout.write("    ");
+        line.push("    ");
 
         // print alphabetical labels along the top
         for (let x in displayGrid[0]) {
-            process.stdout.write(alpha[x] + "  ");
+            //process.stdout.write(alpha[x] + "  ");
+            line.push(alpha[x] + "  ");
         }
+
+        console.log(...line);
         
         // print rows, and row labels
         for (let y in displayGrid) {
+            line = [];
+
             if (y < 10) {
-                process.stdout.write("\n " + y);
+                //process.stdout.write("\n " + y);
+                line.push("\n " + y);
             } else {
-                process.stdout.write("\n" + y);
+                //process.stdout.write("\n" + y);
+                line.push("\n" + y);
             }
-            
 
             for (let x in displayGrid[y]) {
                 if (this.flagMask[y][x] != "F") {
-                    process.stdout.write("  " + displayGrid[y][x]);
+                    //process.stdout.write("  " + displayGrid[y][x]);
+                    line.push("  " + displayGrid[y][x]);
                 } else {
-                    process.stdout.write("  F");
+                    //process.stdout.write("  F");
+                    line.push("  ", flag);
                 }
             }
+            
+            console.log(...line);
         }
     }
 
